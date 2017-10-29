e9c9c880a2a72a67df50adf28e3b654f54cf408f
Mario Škrlec <whitepostmail@gmail.com>
2017-10-29 17:11:45 +0100

    Added LessonView view model

---
+++ lib/Library/LearningMetadata/Business/ViewModel/Lesson/LessonText.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Library\LearningMetadata\Business\ViewModel\Lesson;
+
+class LessonText implements \JsonSerializable
+{
+    /**
+     * @var string $name
+     */
+    private $name;
+    /**
+     * @var string $text
+     */
+    private $text;
+    /**
+     * LessonText constructor.
+     * @param string $name
+     * @param string $text
+     */
+    public function __construct(
+        string $name,
+        string $text
+    ) {
+        $this->name = $name;
+        $this->text = $text;
+    }
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+    /**
+     * @param string $text
+     */
+    public function setText(string $text)
+    {
+        $this->text = $text;
+    }
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->getName(),
+            'text' => $this->getText(),
+        ];
+    }
+    /**
+     * @inheritdoc
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'name' => $this->getName(),
+            'text' => $this->getText(),
+        ];
+    }
+}
---
+++ lib/Library/LearningMetadata/Business/ViewModel/Lesson/LessonView.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Library\LearningMetadata\Business\ViewModel\Lesson;
+
+use Library\Infrastructure\Notation\ArrayNotationInterface;
+
+class LessonView implements \JsonSerializable
+{
+    /**
+     * @var string $internalName
+     */
+    protected $internalName;
+    /**
+     * @var string $name
+     */
+    protected $name;
+    /**
+     * @var Tip[] $tips
+     */
+    protected $tips = [];
+    /**
+     * @var LessonText[] $lessonTexts
+     */
+    protected $lessonTexts = [];
+
+    public function __construct(
+        string $internalName,
+        string $name
+    ) {
+        $this->name = $name;
+        $this->internalName = $internalName;
+    }
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+    /**
+     * @return string
+     */
+    public function getInternalName(): string
+    {
+        return $this->internalName;
+    }
+    /**
+     * @param string $internalName
+     */
+    public function setInternalName(string $internalName)
+    {
+        $this->internalName = $internalName;
+    }
+    /**
+     * @return Tip[]
+     */
+    public function getTips(): array
+    {
+        return $this->tips;
+    }
+    /**
+     * @param Tip[] $tips
+     */
+    public function setTips(array $tips)
+    {
+        $this->tips = $tips;
+    }
+    /**
+     * @return LessonText[]
+     */
+    public function getLessonTexts(): array
+    {
+        return $this->lessonTexts;
+    }
+    /**
+     * @param LessonText[] $lessonTexts
+     */
+    public function setLessonTexts(array $lessonTexts)
+    {
+        $this->lessonTexts = $lessonTexts;
+    }
+    /**
+     * @return array
+     */
+    public function jsonSerialize() : array
+    {
+        $jsonSeriazible =  [
+            'internalName' => $this->getInternalName(),
+            'name' => $this->getName(),
+            'tips' => [],
+            'lessonTexts' => [],
+        ];
+
+        $tips = [];
+        foreach ($this->getTips() as $tip) {
+            $tips[] = (string) $tip;
+        }
+
+        $jsonSeriazible['tips'] = $tips;
+
+        $lessonTexts = [];
+        /** @var ArrayNotationInterface $text */
+        foreach ($lessonTexts as $text) {
+            $lessonTexts[] = $text->toArray();
+        }
+
+        $jsonSeriazible['lessonTexts'] = $lessonTexts;
+
+        return $jsonSeriazible;
+    }
+}

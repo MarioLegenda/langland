95a96c7790bca0cbd026f1662ad926c0461074a7
Mario Škrlec <whitepostmail@gmail.com>
2018-03-14 21:47:01 +0100

    Created business rules for resolving word number

--- lib/PublicApi/LearningSystem/DataDecider/InitialDataDecider.php
+++ lib/PublicApi/LearningSystem/DataDecider/InitialDataDecider.php
@@ -2,10 +2,17 @@
 
 namespace PublicApi\LearningSystem\DataDecider;
 
+use LearningSystem\Infrastructure\Type\ChallengesType;
+use LearningSystem\Infrastructure\Type\FreeTimeType;
 use LearningSystem\Infrastructure\Type\GameType\BasicGameType;
+use LearningSystem\Infrastructure\Type\PersonType;
+use LearningSystem\Infrastructure\Type\SpeakingLanguagesType;
+use LearningSystem\Infrastructure\Type\StressfulJobType;
 use LearningSystem\Library\DataProviderInterface;
+use LearningSystem\Infrastructure\Type\TypeInterface;
 use PublicApi\LearningSystem\DataProvider\Word\ProvidedWordDataCollection;
 use PublicApi\LearningSystem\QuestionAnswersApplicationProvider;
+use PublicApi\LearningUser\Infrastructure\Request\QuestionAnswers;
 
 class InitialDataDecider implements DataDeciderInterface
 {
@@ -36,7 +43,8 @@ class InitialDataDecider implements DataDeciderInterface
     {
         $questionAnswers = $this->questionAnswersApplicationResolver->resolve();
 
-        $wordNumber = 20;
+        $wordNumber = $this->resolveWordNumber($questionAnswers);
+
         $wordLevel = 1;
 
         /** @var ProvidedWordDataCollection $dataCollection */
@@ -49,5 +57,82 @@ class InitialDataDecider implements DataDeciderInterface
             'game_type' => BasicGameType::fromValue(BasicGameType::getName()),
             'data' => $dataCollection,
         ];
+    }
+    /**
+     * @param QuestionAnswers $questionAnswers
+     * @return int
+     */
+    private function resolveWordNumber(QuestionAnswers $questionAnswers): int
+    {
+        $wordNumber = 20;
+
+        foreach ($questionAnswers as $typeName => $answer) {
+            $type = $questionAnswers->asType($typeName);
+
+            $wordNumber = $this->resolveWordNumberForType($type, $wordNumber);
+        }
+
+        if ($wordNumber < 15) {
+            $wordNumber = 15;
+        }
+
+        return $wordNumber;
+    }
+    /**
+     * @param TypeInterface $type
+     * @param int $wordNumber
+     * @return int
+     */
+    private function resolveWordNumberForType(TypeInterface $type, int $wordNumber): int
+    {
+        if ($type instanceof SpeakingLanguagesType) {
+            if ($type->getValue() >= 1) {
+                return ($wordNumber += 3);
+            }
+        }
+
+        if ($type instanceof PersonType) {
+            if ($type->getValue() === 'risk_taker') {
+                return ($wordNumber += 3);
+            }
+
+            if ($type->getValue() === 'sure_thing') {
+                return ($wordNumber -= 2);
+            }
+
+            return $wordNumber;
+        }
+
+        if ($type instanceof FreeTimeType) {
+            if ($type->getValue() === '1_hour') {
+                return ($wordNumber += 3);
+            }
+
+            if ($type->getValue() === '2_hours') {
+                return ($wordNumber += 3);
+            }
+
+            if ($type->getValue() === 'all_time') {
+                return ($wordNumber += 5);
+            }
+        }
+
+        if ($type instanceof ChallengesType) {
+            if ($type->getValue() === 'likes_challenges') {
+                return ($wordNumber += 2);
+            }
+
+            if ($type->getValue() === 'dislike_challenges') {
+                return ($wordNumber -= 2);
+            }
+        }
+
+        if ($type instanceof StressfulJobType) {
+            if ($type->getValue() === 'stressful_job') {
+                return ($wordNumber -= 5);
+            }
+        }
+
+        return $wordNumber;
     }
 }
--- lib/PublicApi/LearningUser/Infrastructure/Request/QuestionAnswers.php
+++ lib/PublicApi/LearningUser/Infrastructure/Request/QuestionAnswers.php
@@ -2,6 +2,9 @@
 
 namespace PublicApi\LearningUser\Infrastructure\Request;
 
+use LearningSystem\Infrastructure\Type\TypeList\FrontendTypeList;
+use LearningSystem\Infrastructure\Type\TypeInterface;
+
 class QuestionAnswers implements \IteratorAggregate
 {
     /**
@@ -22,6 +25,30 @@ class QuestionAnswers implements \IteratorAggregate
     public function getAnswers(): array
     {
         return $this->answers;
+    }
+
+    /**
+     * @return TypeInterface[]
+     */
+    public function getAnswersAsTypes(): array
+    {
+        $typeList = FrontendTypeList::getList();
+
+        $types = [];
+
+        foreach ($this->answers as $typeName => $answer) {
+            $types[] = $typeList[$typeName]::{'fromValue'}($answer);
+        }
+
+        return $types;
+    }
+    /**
+     * @param string $typeName
+     * @return TypeInterface
+     */
+    public function asType(string $typeName): TypeInterface
+    {
+        return FrontendTypeList::getList()[$typeName]::{'fromValue'}($this->answers[$typeName]);
     }
     /**
      * @return \ArrayIterator
